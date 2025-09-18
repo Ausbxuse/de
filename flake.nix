be8ffee63e458ac1ea7bf4f34f7371b08a5e403c
Zhenyu Zhao <peterzhaozzy@gmail.com>
2025-09-17 21:19:55 -0700

    fix home

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Custom fonts, themes, apps, scripts as a Home-Manager module";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
   outputs = {self, ...}: {
     homeManagerModules.default = {
       lib,
@@ -26,42 +27,36 @@
       };
 
       config = {
-        # Drop xdg.* entirely
-        # xdg.enable = lib.mkDefault true;
+        # xdg.* deliberately not used
 
         home.sessionPath = ["${config.home.homeDirectory}/.local/bin"];
 
         # Data dirs under ~/.local/share/*
         home.file = lib.mkMerge [
-          # Fonts -> ~/.local/share/fonts
           (mkIf config.myFonts.enable {
             ".local/share/fonts" = {
               source = ./fonts;
               recursive = true;
             };
           })
-          # Themes -> ~/.local/share/themes
           (mkIf config.myThemes.enable {
             ".local/share/themes" = {
               source = ./themes;
               recursive = true;
             };
           })
-          # Desktop files -> ~/.local/share/applications
           (mkIf config.myApps.enable {
             ".local/share/applications" = {
               source = ./applications;
               recursive = true;
             };
           })
-          # Dictionaries -> ~/.local/share/stardict
           (mkIf config.myDict.enable {
             ".local/share/stardict" = {
               source = ./stardict;
               recursive = true;
             };
           })
-          # Wallpapers -> ~/.local/share/wallpapers
           (mkIf config.myWallpapers.enable {
             ".local/share/wallpapers" = {
               source = ./wallpapers;
@@ -70,7 +65,7 @@
           })
         ];
 
-        # Binaries -> ~/.local/bin (symlinkJoin is fine)
+        # Binaries -> ~/.local/bin (separate from the mkMerge above)
         home.file.".local/bin" = mkIf config.myScripts.enable {
           source = pkgs.symlinkJoin {
             name = "custom-bin";
