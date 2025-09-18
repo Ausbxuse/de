2a9c42af3ec3a0fb885285f69d12d45cc81ffaa7
Zhenyu Zhao <peterzhaozzy@gmail.com>
2025-09-17 21:01:38 -0700

    fix errors

--- flake.nix
+++ flake.nix
@@ -1,50 +1,48 @@
 {
   description = "Custom fonts, themes, apps, scripts as a Home-Manager module";
 
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   outputs = {self, ...}: {
     homeManagerModules.default = {
       lib,
       pkgs,
       config,
       ...
-    }: let
-      cfg = config.my;
-    in {
-      options.my = {
-        enable = lib.mkEnableOption "Enable my extras";
-        host = lib.mkOption {
+    }: {
+      options = {
+        myHost = lib.mkOption {
           type = lib.types.nullOr lib.types.str;
           default = null; # e.g., "timy" / "uni"
           description = "Optional per-host selector for extra scripts.";
         };
-        fonts.enable = lib.mkEnableOption "Install custom fonts";
-        themes.enable = lib.mkEnableOption "Install custom themes";
-        apps.enable = lib.mkEnableOption "Install custom applications/configs";
-        scripts.enable = lib.mkEnableOption "Install custom scripts";
-        dict.enable = lib.mkEnableOption "Install custom dictionaries";
-        wallpapers.enable = lib.mkEnableOption "Install wallpapers";
+        myFonts.enable = lib.mkEnableOption "Install custom fonts";
+        myThemes.enable = lib.mkEnableOption "Install custom themes";
+        myApps.enable = lib.mkEnableOption "Install custom applications/configs";
+        myScripts.enable = lib.mkEnableOption "Install custom scripts";
+        myDict.enable = lib.mkEnableOption "Install custom dictionaries";
+        myWallpapers.enable = lib.mkEnableOption "Install wallpapers";
       };
 
-      config = lib.mkIf cfg.enable {
+      config = {
         xdg.enable = lib.mkDefault true;
 
         home.sessionPath = ["${config.home.homeDirectory}/.local/bin"];
 
         xdg.dataFile = lib.mkMerge [
-          (lib.mkIf cfg.fonts.enable {"fonts".source = ./fonts;})
-          (lib.mkIf cfg.themes.enable {"themes".source = ./themes;})
-          (lib.mkIf cfg.apps.enable {"applications".source = ./applications;})
-          (lib.mkIf cfg.dict.enable {"stardict".source = ./stardict;})
-          (lib.mkIf cfg.wallpapers.enable {"wallpapers".source = ./wallpapers;})
+          (lib.mkIf config.myFonts.enable {"fonts".source = ./fonts;})
+          (lib.mkIf config.myThemes.enable {"themes".source = ./themes;})
+          (lib.mkIf config.myApps.enable {"applications".source = ./applications;})
+          (lib.mkIf config.myDict.enable {"stardict".source = ./stardict;})
+          (lib.mkIf config.myWallpapers.enable {"wallpapers".source = ./wallpapers;})
         ];
 
-        home.file.".local/bin" = lib.mkIf cfg.scripts.enable {
+        home.file.".local/bin" = lib.mkIf config.my.scripts.enable {
           source = pkgs.symlinkJoin {
             name = "custom-bin";
             paths =
               [./bin]
-              ++ lib.optional (cfg.host == "timy") ./bin-timy
-              ++ lib.optional (cfg.host == "uni") ./bin-uni;
+              ++ lib.optional (config.my.host == "timy") ./bin-timy
+              ++ lib.optional (config.my.host == "uni") ./bin-uni;
           };
           recursive = true;
           # executable = true  # not needed for directories
